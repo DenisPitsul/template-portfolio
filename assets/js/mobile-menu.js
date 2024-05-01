536140dc8d81c62e3ae126f9a04884b9b065d353
DenisPitsul <denia.pitsul@gmail.com>
2024-05-01 19:12:21 +0300

    Rewrite semanticaly correctly

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,12 +1,12 @@
 (() => {
-    const mobileMenu = document.querySelector('#nav');
+    const mobileMenu = document.querySelector('#sideMenu');
     const openMenuBtn = document.querySelector('#burgerBtn');
     const overlay = document.querySelector('#overlay');
     const closeBtn = document.querySelector('#closeBtn');
   
     const toggleMenu = (e) => {
       e.preventDefault();  
-      mobileMenu.classList.toggle('is-nav-open');
+      mobileMenu.classList.toggle('is-side-menu-open');
       overlay.classList.toggle('is-open');
     };
   
@@ -17,7 +17,7 @@
     // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia('(min-width: 993px)').addEventListener('change', e => {
       if (!e.matches) return;
-      mobileMenu.classList.remove('is-nav-open');
+      mobileMenu.classList.remove('is-side-menu-open');
       overlay.classList.remove('is-open');
     });
   })();
